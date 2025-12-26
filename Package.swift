6053e11ae2c0ba3b3da6722bddff0ad347bab3b2
thisisaaronland <thisisaaronland@localhost>
2025-12-26 14:38:56 -0800

    initial commit; compiles but completely untested

---
+++ Package.swift
@@ -0,0 +1,29 @@
+// swift-tools-version: 6.2
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "MobileCLIP",
+    platforms: [
+        .macOS(.v15)
+    ],
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "MobileCLIP",
+            targets: ["MobileCLIP"]
+        ),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "MobileCLIP"
+        ),
+        .testTarget(
+            name: "MobileCLIPTests",
+            dependencies: ["MobileCLIP"]
+        ),
+    ]
+)
